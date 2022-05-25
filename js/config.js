89da118011f523e2c5417d7a07423e3f2f9f723e
PedrinelliLorenzo <lollypedro1@gmail.com>
2022-05-25 14:52:10 +0200

    fetch 3

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const API_KEY = "a862a232";
+export const BASE_URL = `https://omdbapi.com/?apikey=${API_KEY}&`;
+
+export const CONTENT_TYPE = {
+MOVIE: "movie",
+SERIES: "series",
+EPISODE: "episode",
+GAME: "game"
+}
--- js/modello-api.js
+++ js/modello-api.js
@@ -1,21 +1,11 @@
-const URL_Ghost_rider = "https://omdbapi.com/?apikey=fb7170de&s=ghost_rider";
+import {BASE_URL} from "./config.js";
 
-export const listGhost = () => {
-    fetch(URL_Ghost_rider)
+export const apilist = (s, type) => {
+    const url = BASE_URL + `s=${s}&type=${type}`;
+    fetch(url)
     .then((response) => response.json())
     .then((results) => {
-        const movies = results.Search;
-        console.log(movies);
-    });
-};
-
-const URL_The_Punisher = "https://omdbapi.com/?apikey=fb7170de&s=the_punisher&type=series";
-
-export const listPunisher = () => {
-    fetch(URL_The_Punisher)
-    .then((responseP) => responseP.json())
-    .then((resultsP) => {
-        const series = resultsP.Search;
-        console.log(series);
+        const items = results.Search;
+        console.log(items);
     });
 };
